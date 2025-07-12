78e8331623edde34b4db611f03378d8e966f94d9
moriken-forest <129244811+moriken-forest@users.noreply.github.com>
2025-07-12 19:54:58 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CalendarView from '../views/CalendarView.vue'
-import ListView from '../views/ListView.vue'
+import ListView     from '../views/ListView.vue'
 
 const routes = [
-  { path: '/', redirect: '/calendar' },
+  { path: '/',        redirect: '/calendar' },
   { path: '/calendar', component: CalendarView },
-  { path: '/list', component: ListView }
+  { path: '/list',     component: ListView }
 ]
 
 export default createRouter({
