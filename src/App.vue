5ea913f60615a5452e6d1f92b6edb124cb9703e2
moriken-forest <129244811+moriken-forest@users.noreply.github.com>
2025-07-14 12:01:25 +0900

    ユーザー選択メニューの追加

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,12 @@
         <nav>
           <router-link to="/calendar"><span class="material-icons">calendar_month</span></router-link>
           <router-link to="/list"><span class="material-icons">list</span></router-link>
-          <a href="#" @click.prevent="switchUser">
+          <a href="#" @click.prevent="toggleUserMenu">
             <span class="material-icons">person</span>
           </a>
+          <div v-if="userMenu" class="user-menu">
+            <button v-for="u in users" :key="u" @click="selectUser(u)">{{ u }}</button>
+          </div>
           <a href="#" @click.prevent="clearCache"><span class="material-icons">refresh</span></a>
         </nav>
     </header>
@@ -18,17 +21,27 @@
 </template>
 
 <script>
-import { toggleUser } from './utils/user'
+import { getUsers, setUser } from './utils/user'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      userMenu: false,
+      users: getUsers()
+    }
+  },
   methods: {
     clearCache() {
       localStorage.clear()
       location.reload()
     },
-    switchUser() {
-      toggleUser()
+    toggleUserMenu() {
+      this.userMenu = !this.userMenu
+    },
+    selectUser(user) {
+      setUser(user)
+      this.userMenu = false
       this.clearCache()
     }
   }
