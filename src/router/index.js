b96cee2e34377f97f331f44eb370eb4a6b165b0b
moriken-forest <129244811+moriken-forest@users.noreply.github.com>
2025-07-16 19:17:14 +0900

    利用ガイドページを追加

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import CalendarView    from '../views/CalendarView.vue'
 import ListView        from '../views/ListView.vue'
 import RegisterView    from '../views/RegisterView.vue'
 import CategoryView    from '../views/CategoryView.vue'
+import ChatGPTGuideView from '../views/ChatGPTGuideView.vue'
 
 const routes = [
   { path: '/',        redirect: '/calendar' },
@@ -10,6 +11,7 @@ const routes = [
   { path: '/list',     component: ListView },
   { path: '/category/:base', component: CategoryView },
   { path: '/register', component: RegisterView },
+  { path: '/guide', component: ChatGPTGuideView },
 ]
 
 export default createRouter({
