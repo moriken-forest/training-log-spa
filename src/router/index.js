75751c5e775d4c49a31f6ed6224f9d736760ed16
moriken-forest <129244811+moriken-forest@users.noreply.github.com>
2025-07-17 19:10:24 +0900

    タグクリック時はリスト画面で絞り込み

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import CalendarView    from '../views/CalendarView.vue'
 import ListView        from '../views/ListView.vue'
 import RegisterView    from '../views/RegisterView.vue'
-import CategoryView    from '../views/CategoryView.vue'
 import ChatGPTGuideView from '../views/ChatGPTGuideView.vue'
 
 const routes = [
   { path: '/',        redirect: '/calendar' },
   { path: '/calendar', component: CalendarView },
   { path: '/list/:base?', component: ListView },
-  { path: '/category/:base?', component: CategoryView },
   { path: '/register', component: RegisterView },
   { path: '/guide', component: ChatGPTGuideView },
 ]
